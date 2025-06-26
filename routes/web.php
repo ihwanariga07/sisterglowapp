6a5cc9481777f6a8d6b3c443bfee433768a58e59
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-06-27 01:01:04 +0700

    memperbaiki web.php untuk costumers

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\CustomerController; 
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/home', function () {
     return view('home');
@@ -23,12 +12,9 @@ Route::get('/cektemplate', function () {
     return view('layouts.template');
 });
 
-
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Route::get('/costumers', [karyawanController::class, 'index']);
 
 Route::resource('customers', CustomerController::class);
 Route::get('/costumers', [CustomerController::class, 'index'])->name('costumers.index');
