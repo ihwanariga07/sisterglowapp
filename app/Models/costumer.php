f1cdae30fea670f000cc8c8ce319ace1bb4f46f9
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-06-27 01:30:48 +0700

    merubah code costumer model

--- app/Models/costumer.php
+++ app/Models/costumer.php
@@ -2,18 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Costumer extends Model
 {
+    use HasFactory;
+
     protected $table = 'tbl_costumers'; 
-    protected $primaryKey = 'id_costumers';
-    public $incrementing = false;
-    protected $keyType = 'string';
+    protected $primaryKey = 'id_costumers'; 
+    public $incrementing = false; 
+    protected $keyType = 'string'; 
 
     protected $fillable = [
-        'id_costumers', 'nama', 'no_hp', 'email'
+        'id_costumers',
+        'nama',
+        'no_hp',
+        'email'
     ];
-
-   
 }
